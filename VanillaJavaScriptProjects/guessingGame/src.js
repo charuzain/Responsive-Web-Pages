3865559c1641fd4f34e220f0c4d03e438fe9a0dd
charuzain <charuabhizain@gmail.com>
2023-03-08 10:21:05 -0500

    decrease score by one

--- VanillaJavaScriptProjects/guessingGame/src.js
+++ VanillaJavaScriptProjects/guessingGame/src.js
@@ -1,23 +1,40 @@
 window.addEventListener('DOMContentLoaded', () => {
   // generate randon number 
-  const randomNum = Math.floor(Math.random()*20 +1)
+  
+  const randomNum = Math.floor(Math.random() * 20 + 1)
   console.log(randomNum)
 
 
-  const display = document.querySelector(".num").textContent
-  
+  const display = document.querySelector(".num")
+  const msg = document.querySelector(".msg")
   const result = document.querySelector('.result')
-  console.log(result)
-
-
+  // console.log(result)
+  const score = document.querySelector('.score-val')
   const guessedNum = document.querySelector('#guessedNumber')
- 
   const checkBtn = document.querySelector('#check-btn')
+
   
 
   checkBtn.addEventListener('click', () => {
-    console.log("btn clicked")
-    console.log(guessedNum.value)
+    score.textContent = Number(score.textContent) - 1
+
+    if (Number(score.textContent) === 0) {
+      msg.textContent = "Game Over !!!"
+    }
+      const guessedNumVal = Number(guessedNum.value)
+      if (guessedNum.value > randomNum) {
+        result.textContent = "📈 Too High"
+      }
+      else if (guessedNum.value < randomNum) {
+        result.textContent = "📉 Too Low"
+      }
+      else {
+        result.textContent = "🎊✌🏻 You Won !!!!"
+        console.log(display)
+        display.textContent = guessedNumVal
+        console.log(display)
+      }
+    
   })
  
 
