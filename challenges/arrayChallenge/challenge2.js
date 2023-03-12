0fa8c696a3c9ebcc9bbc86cc73267ec77867ff16
charuzain <charuabhizain@gmail.com>
2023-03-11 23:19:45 -0500

    Reduce & Map Challenge

---
+++ challenges/arrayChallenge/challenge2.js
@@ -0,0 +1,12 @@
+const eurotoToUSD = 1.1 
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const movementsInUSD = movements.map(move => move * eurotoToUSD)
+console.log(movementsInUSD)
+
+
+const totalBalance = (movements) => movements.reduce((a, c) => a + c)
+
+
+console.log(totalBalance(movements))
